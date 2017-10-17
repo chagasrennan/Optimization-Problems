7b1d5694111586d0ef87b0a6f2be4766bb9d199e
chagasrennan <chagasrennan@gmail.com>
2017-10-17 15:23:29 -0400

    Formulation Validated
    Model formulated with gurobipy, running and returning solution. Next step is generate graphical solution

--- VRPTW/VRPTW.py
+++ VRPTW/VRPTW.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
 # -- coding: utf-8 --
 
+'''
+VRPTW - Vehicle Routing Problem with Time Windows
+
+
+
+'''
+
 from __future__ import (absolute_import, division, print_function,
     generators, nested_scopes, unicode_literals, with_statement)
 
-# ==================================================================
-#     Python code to solve simple Vehicle Routing Problem
+# ===================================================================
+#     Python code to solve Vehicle Routing Problem with Time Windows
 #     For this project we used Gurobi® for academic purposes  
 
 #  Author: Rennan Danilo Seimetz Chagas
 #  email: chagasrennan@gmail.com
-# ==================================================================
+# ===================================================================
 
 
 from gurobipy import *
 
-with open("./solomon_25/toy.txt", 'r') as f:
+with open("./solomon_25/C102.txt", 'r') as f:
     lines = f.readlines()
 
 instname = lines[0]
@@ -26,8 +33,7 @@ cap = map(int,lines[4].split())[1]
 rgcli = range(1, ncli+1)
 cli = list(range(1, ncli+1))      # list of clients
 nodes = list(range(ncli+2))       # list of clients
-# veh = list(rgcli)               # list of vehicles
-veh = list(range(2))              # list of vehicles
+veh = list(rgcli)               # list of vehicles
 
 # clients coordinates
 xycli = {i:map(int,(lines[9+i].split()[1],lines[9+i].split()[2])) for i in nodes[:-1]}
@@ -39,19 +45,23 @@ demand.append(demand[0])
 
 # release date time window
 rd = map(int,[lines[9+i].split()[4] for i in nodes[:-1]])
+rd.append(0)
 
 # due date time window
 dd = map(int,[lines[9+i].split()[5] for i in nodes[:-1]])
+dd.append(float('inf'))
 
 # service time
 sv = map(int,[lines[9+i].split()[6] for i in nodes[:-1]])
 sv.append(sv[0])
 
 # costs ci,j
-c = [[((xycli[i][0]-xycli[j][0])**2 + (xycli[i][1]-xycli[j][1])**2)**0.5 for i in nodes[:-1]] for j in nodes[:-1]]
-c.append(c[0])
-
+c = [[((xycli[i][0]-xycli[j][0])**2 + (xycli[i][1]-xycli[j][1])**2)**0.5 for i in nodes] for j in nodes]
+# c.append(c[0])
+print
 bigM = 9999
+lbigm = [[c[i][j] + dd[i] - rd[j] for i in nodes[:-1]] for j in nodes[:-1]]
+# print(lbigm)
 
 # create model 
 m = Model('vrptw')
@@ -68,10 +78,9 @@ s = m.addVars(nodes, veh,
                 name='s',
                 vtype=GRB.CONTINUOUS)
 
-obj = quicksum(x[i,j,k]*c[i][j] for i in cli for j in cli for k in veh )
 # Objetctive Function
 m.setObjective(
-    quicksum(x[i,j,k]*c[i][j] for i in cli for j in cli for k in veh if i!=j),
+    quicksum(x[i,j,k]*c[i][j] for i in nodes for j in nodes for k in veh if i!=j),
     # obj,
     GRB.MINIMIZE
 )
@@ -102,20 +111,20 @@ m.addConstrs(
     (quicksum(x[i,nodes[-1],k] for i in nodes if i!= nodes[-1]) == 1 for k in veh)
 )
 
-# m.addConstrs(
-#     # the relationship between the vehicle depar- ture time from a customer and its immediate successor
-#     (s[i,k] + c[i][j] - bigM*(1 - x[i,j,k]) - s[j,k] <= 0 for i in nodes for j in nodes for k in veh)
-# )
+m.addConstrs(
+    # the relationship between the vehicle depar- ture time from a customer and its immediate successor
+    (s[i,k] + c[i][j] - bigM*(1 - x[i,j,k]) - s[j,k] <= 0 for i in nodes for j in nodes for k in veh)
+)
 
-# m.addConstrs(
-#     # time windows are observed
-#     (rd[i] - s[i,k] <= 0 for i in nodes[:-1] for k in veh)
-# )
+m.addConstrs(
+    # time windows are observed
+    (rd[i] - s[i,k] <= 0 for i in nodes[:-1] for k in veh)
+)
 
-# m.addConstrs(
-#     # time windows are observed
-#     (s[i,k] - dd[i] <= 0 for i in nodes[:-1] for k in veh)
-# )
+m.addConstrs(
+    # time windows are observed
+    (s[i,k] - dd[i] <= 0 for i in nodes[:-1] for k in veh)
+)
 
 # Solve
 m.optimize()
