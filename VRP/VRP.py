387f6760cc287b9cb9a8857c09f7a15b6c2bc623
chagasrennan <chagasrennan@gmail.com>
2017-10-17 13:14:55 -0400

    parser and most of model structure
    still sumw bugs on the formulation

--- VRP/VRP.py
+++ VRP/VRP.py
@@ -15,7 +15,7 @@ from __future__ import (absolute_import, division, print_function,
 
 from gurobipy import *
 
-with open("./solomon_25/C101.txt", 'r') as f:
+with open("./solomon_25/toy.txt", 'r') as f:
     lines = f.readlines()
 
 instname = lines[0]
@@ -23,53 +23,109 @@ instname = lines[0]
 ncli = map(int,lines[4].split())[0]
 cap = map(int,lines[4].split())[1]
 
-rgcli = range(ncli+1)
-
-cli = list(rgcli)       # list of clients
-veh = list(rgcli)       # list of vehicles
+rgcli = range(1, ncli+1)
+cli = list(range(1, ncli+1))      # list of clients
+nodes = list(range(ncli+2))       # list of clients
+# veh = list(rgcli)               # list of vehicles
+veh = list(range(2))              # list of vehicles
 
 # clients coordinates
-xycli = {i:map(int,(lines[9+i].split()[1],lines[9+i].split()[2])) for i in rgcli}
+xycli = {i:map(int,(lines[9+i].split()[1],lines[9+i].split()[2])) for i in nodes[:-1]}
+xycli[nodes[-1]] = xycli[0]
 
 # demands per clients
-demand = map(int,[lines[9+i].split()[3] for i in rgcli])
+demand = map(int,[lines[9+i].split()[3] for i in nodes[:-1]])
+demand.append(demand[0])
 
 # release date time window
-rd = map(int,[lines[9+i].split()[4] for i in rgcli])
+rd = map(int,[lines[9+i].split()[4] for i in nodes[:-1]])
 
 # due date time window
-dd = map(int,[lines[9+i].split()[5] for i in rgcli])
+dd = map(int,[lines[9+i].split()[5] for i in nodes[:-1]])
 
 # service time
-sv = map(int,[lines[9+i].split()[6] for i in rgcli])
+sv = map(int,[lines[9+i].split()[6] for i in nodes[:-1]])
+sv.append(sv[0])
 
 # costs ci,j
-c = [ [((xycli[i][0]-xycli[j][0])**2 + (xycli[i][1]-xycli[j][1])**2)**0.5 for i in rgcli] for j in rgcli]
+c = [[((xycli[i][0]-xycli[j][0])**2 + (xycli[i][1]-xycli[j][1])**2)**0.5 for i in nodes[:-1]] for j in nodes[:-1]]
+c.append(c[0])
+
+bigM = 9999
 
 # create model 
 m = Model('vrptw')
 
 # create variable Xi,j,k
 # 1, if vehicle k drives directly from vertex i to vertex j 
-x = m.addVars(cli,cli, veh, 
+x = m.addVars(nodes, nodes, veh, 
                 name='x',
                 vtype=GRB.BINARY) 
-# print(x)
 
 # variable si,k
 # the time vehicle k starts to service customer i.
-s = m.addVars(cli, veh, 
+s = m.addVars(nodes, veh, 
                 name='s',
                 vtype=GRB.CONTINUOUS)
-# print(s)
+
+obj = quicksum(x[i,j,k]*c[i][j] for i in cli for j in cli for k in veh )
+# Objetctive Function
+m.setObjective(
+    quicksum(x[i,j,k]*c[i][j] for i in cli for j in cli for k in veh if i!=j),
+    # obj,
+    GRB.MINIMIZE
+)
 
 # Constraints
 m.addConstrs(
-    x.sum(i,'*','*') == 1 for i in cli if i!=j
-) 
+    # each customer is visited exactly once
+    (quicksum(x[i,j,k] for j in nodes for k in veh if j!=i) == 1 for i in cli) 
+)  
 
 m.addConstrs(
-    quicksum(demand[i]*(quicksum(x[i,j,k]) for j in cli) for i in cli) <= cap
+    # a vehicle can only be loaded up to it's capacity
+    (quicksum(x[i,j,k]*demand[i] for i in cli for j in nodes if i!=j) <= cap for k in veh)
 )
 
+m.addConstrs(
+    # each vehicle must leave the depot 0
+    (quicksum(x[0,j,k] for j in nodes) == 1 for k in veh)
+)
 
+m.addConstrs(
+    # after a vehicle arrives at a customer it has to leave for another destination
+    (quicksum(x[i,h,k] - x[h,j,k] for i in nodes for j in nodes if i!=j) == 0 for h in cli for k in veh)
+)
+
+m.addConstrs(
+    # all vehicles must arrive at the depot n + 1. The inequalities
+    (quicksum(x[i,nodes[-1],k] for i in nodes if i!= nodes[-1]) == 1 for k in veh)
+)
+
+# m.addConstrs(
+#     # the relationship between the vehicle depar- ture time from a customer and its immediate successor
+#     (s[i,k] + c[i][j] - bigM*(1 - x[i,j,k]) - s[j,k] <= 0 for i in nodes for j in nodes for k in veh)
+# )
+
+# m.addConstrs(
+#     # time windows are observed
+#     (rd[i] - s[i,k] <= 0 for i in nodes[:-1] for k in veh)
+# )
+
+# m.addConstrs(
+#     # time windows are observed
+#     (s[i,k] - dd[i] <= 0 for i in nodes[:-1] for k in veh)
+# )
+
+# Solve
+m.optimize()
+
+m.write('vrptw.lp')
+
+print('Objetivo: ', m.objVal)
+
+for i in nodes:
+    for j in nodes:
+        for k in veh:
+            if x[i,j,k].x > 0.001:
+                print('x[',i,',',j,',',k,']=', x[i,j,k])
